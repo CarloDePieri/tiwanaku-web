d835d0b7f26b99af99ccd23eb18a80279ffbed82
Carlo De Pieri <depieri.carlo@gmail.com>
2024-06-29 16:44:37 +0200

    refactor controls ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,11 @@
 import "./App.css"
-import { useState } from "react"
-import { useAppDispatch, useAppSelector } from "./app/hooks.ts"
 import Board from "./components/Board.tsx"
-import {
-  abortNewBoardGeneration,
-  generateNewBoard,
-  GenerateNewBoardPromise,
-  selectGeneratingBoard,
-} from "./game/gameSlice.ts"
-import { BoardSize } from "./game/GameBoard.ts"
+import { Controls } from "./components/Controls.tsx"
 
 function App() {
-  const [promise, setPromise] = useState<GenerateNewBoardPromise | null>(null)
-
-  const loading = useAppSelector(selectGeneratingBoard)
-  const dispatch = useAppDispatch()
-
-  function abortGeneration() {
-    // stop the current generation
-    if (promise) abortNewBoardGeneration(promise)
-  }
-
-  const generate = (size: BoardSize) => {
-    // if there was a previous request, cancel it
-    if (loading) {
-      abortGeneration()
-    }
-    setPromise(dispatch(generateNewBoard(size)))
-  }
-
   return (
     <div>
-      <div>
-        <button onClick={() => generate("small")}>small</button>
-        <button onClick={() => generate("standard")}>standard</button>
-        {loading && (
-          <>
-            <span> Generating... </span>
-            <button onClick={() => abortGeneration()}>Abort</button>
-          </>
-        )}
-      </div>
+      <Controls />
       <Board />
     </div>
   )
---
+++ src/components/Controls.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import { Button, Col, Container, Row } from "react-bootstrap"
+import { useAppDispatch, useAppSelector } from "../app/hooks.ts"
+import { BoardSize } from "../game/GameBoard.ts"
+import {
+  abortNewBoardGeneration,
+  generateNewBoard,
+  GenerateNewBoardPromise,
+  selectGeneratingBoard,
+} from "../game/gameSlice.ts"
+
+export function Controls() {
+  const [promise, setPromise] = useState<GenerateNewBoardPromise | null>(null)
+
+  const loading = useAppSelector(selectGeneratingBoard)
+  const dispatch = useAppDispatch()
+
+  function abortGeneration() {
+    // stop the current generation
+    if (promise) abortNewBoardGeneration(promise)
+  }
+
+  const generate = (size: BoardSize) => {
+    // if there was a previous request, cancel it
+    if (loading) {
+      abortGeneration()
+    }
+    setPromise(dispatch(generateNewBoard(size)))
+  }
+
+  const loadingMessage = (
+    <div className={"mx-auto controlsText"}>
+      {loading ? "Generating a new board..." : ""}
+    </div>
+  )
+
+  const controls = loading ? (
+    <Button
+      variant={"danger"}
+      className={"controls"}
+      onClick={() => abortGeneration()}
+    >
+      Abort
+    </Button>
+  ) : (
+    <>
+      <Button className={"controls"} onClick={() => generate("small")}>
+        Small
+      </Button>
+      <Button className={"controls"} onClick={() => generate("standard")}>
+        Standard
+      </Button>
+    </>
+  )
+
+  return (
+    <Container style={{ marginTop: "10pt" }}>
+      <Row className="justify-content-md-center">
+        <Col md={"4"} className={"d-flex align-items-center"}>
+          <div className={"mx-auto controlsText"}>Generate a new board</div>
+        </Col>
+        <Col md={"4"} className={"d-flex align-items-center"}>
+          <div className={"mx-auto"}>{controls}</div>
+        </Col>
+        <Col md={"4"} className={"d-flex align-items-center"}>
+          {loadingMessage}
+        </Col>
+      </Row>
+    </Container>
+  )
+}
