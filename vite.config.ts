e47964e3a0b20868d954776a4b18c73221f663c6
Carlo De Pieri <depieri.carlo@gmail.com>
2024-07-04 10:23:26 +0200

    fix vite base path, fix page title

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+  base: "/tiwanaku-web/",
   plugins: [react()],
 })
